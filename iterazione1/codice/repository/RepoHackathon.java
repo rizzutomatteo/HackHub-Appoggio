f767f353593f7dc7508c56e165959c60f433d30b
alemozzoni <alemozzoni@gmail.com>
2026-07-01 14:15:00 +0200

    feat(repository): estende RepoUtente e RepoHackathon per accesso e consultazione

--- iterazione1/codice/repository/RepoHackathon.java
+++ iterazione1/codice/repository/RepoHackathon.java
@@ -2,7 +2,12 @@ package it.unicam.cs.ids.hackhub.repository;
 
 import it.unicam.cs.ids.hackhub.entity.Hackathon;
 
+import java.util.List;
+
 public interface RepoHackathon {
 
     void salva(Hackathon h);
+
+    // UC21: tutti gli hackathon (per la consultazione pubblica del Visitatore).
+    List<Hackathon> tutti();
 }
--- iterazione1/codice/repository/RepoUtente.java
+++ iterazione1/codice/repository/RepoUtente.java
@@ -5,4 +5,13 @@ import it.unicam.cs.ids.hackhub.entity.Utente;
 public interface RepoUtente {
 
     boolean appartieneAdUnTeam(Utente u);
+
+    // UC19: l'email e' univoca (identita' dell'utente).
+    boolean esisteEmail(String email);
+
+    // UC20: recupera l'utente dalle credenziali (null se inesistente).
+    Utente trovaPerEmail(String email);
+
+    // UC19: persiste il nuovo Utente registrato.
+    void salva(Utente u);
 }
