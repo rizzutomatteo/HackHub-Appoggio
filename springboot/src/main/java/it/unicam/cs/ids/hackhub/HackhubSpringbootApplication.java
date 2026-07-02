938e186569dfd2189f192d305333aaa8881ef86e
alemozzoni <alemozzoni@gmail.com>
2026-07-03 00:46:00 +0200

    feat(springboot): classe main @SpringBootApplication

---
+++ springboot/src/main/java/it/unicam/cs/ids/hackhub/HackhubSpringbootApplication.java
@@ -0,0 +1,13 @@
+package it.unicam.cs.ids.hackhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackhubSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackhubSpringbootApplication.class, args);
+	}
+
+}
