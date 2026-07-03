ee5a3d6abf8a8544cbaefdb63e6828c5d89551b5
alessandroacciarresi <aleacc2004@gmail.com>
2026-07-03 09:46:00 +0200

    port(springboot): entita Incarico

---
+++ springboot/src/main/java/it/unicam/cs/ids/hackhub/entity/Incarico.java
@@ -0,0 +1,29 @@
+package it.unicam.cs.ids.hackhub.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+public class Incarico {
+
+    private final Utente utente;
+    private final Hackathon hackathon;
+    private final RuoloStaff ruolo;
+
+    public Incarico(Utente utente, Hackathon hackathon, RuoloStaff ruolo) {
+        this.utente = utente;
+        this.hackathon = hackathon;
+        this.ruolo = ruolo;
+    }
+
+    public Utente getUtente() {
+        return utente;
+    }
+
+    @JsonIgnore
+    public Hackathon getHackathon() {
+        return hackathon;
+    }
+
+    public RuoloStaff getRuolo() {
+        return ruolo;
+    }
+}
