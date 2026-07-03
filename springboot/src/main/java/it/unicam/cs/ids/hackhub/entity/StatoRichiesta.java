bd7f018e3c2d2ace844debce342af556034cdd51
alemozzoni <alemozzoni@gmail.com>
2026-07-03 15:10:00 +0200

    port(springboot): entita StatoRichiesta

---
+++ springboot/src/main/java/it/unicam/cs/ids/hackhub/entity/StatoRichiesta.java
@@ -0,0 +1,8 @@
+package it.unicam.cs.ids.hackhub.entity;
+
+// Stato di RichiestaSupporto: enum semplice (NON pattern State; lo State resta sull'Hackathon).
+public enum StatoRichiesta {
+    Aperta,
+    Gestita,
+    Chiusa
+}
