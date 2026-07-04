81da06aa7369e49a708f662778cc2ffa97024828
alemozzoni <alemozzoni@gmail.com>
2026-07-04 10:31:00 +0200

    port(springboot): stato InCorso

---
+++ springboot/src/main/java/it/unicam/cs/ids/hackhub/state/InCorso.java
@@ -0,0 +1,24 @@
+package it.unicam.cs.ids.hackhub.state;
+
+public class InCorso extends Stato {
+
+    @Override
+    public boolean iscrizioneConsentita() {
+        return false;
+    }
+
+    @Override
+    public boolean sottomissioneConsentita() {
+        return true;
+    }
+
+    @Override
+    public boolean valutazioneConsentita() {
+        return false;
+    }
+
+    @Override
+    public Stato iniziaValutazione() {
+        return new InValutazione();
+    }
+}
