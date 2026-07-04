3b79d85b74f885fa75f1e09b04c4e4439d500e3f
alemozzoni <alemozzoni@gmail.com>
2026-07-04 10:58:00 +0200

    port(springboot): stato InIscrizione

---
+++ springboot/src/main/java/it/unicam/cs/ids/hackhub/state/InIscrizione.java
@@ -0,0 +1,24 @@
+package it.unicam.cs.ids.hackhub.state;
+
+public class InIscrizione extends Stato {
+
+    @Override
+    public boolean iscrizioneConsentita() {
+        return true;
+    }
+
+    @Override
+    public boolean sottomissioneConsentita() {
+        return false;
+    }
+
+    @Override
+    public boolean valutazioneConsentita() {
+        return false;
+    }
+
+    @Override
+    public Stato avvia() {
+        return new InCorso();
+    }
+}
