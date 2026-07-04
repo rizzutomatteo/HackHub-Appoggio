741a985743d45c01e8977fd982ac32c3780e499c
alessandroacciarresi <aleacc2004@gmail.com>
2026-07-04 08:16:00 +0200

    port(springboot): control @Service ServiceSottomissione

---
+++ springboot/src/main/java/it/unicam/cs/ids/hackhub/service/ServiceSottomissione.java
@@ -0,0 +1,43 @@
+package it.unicam.cs.ids.hackhub.service;
+
+import org.springframework.stereotype.Service;
+
+import it.unicam.cs.ids.hackhub.entity.Iscrizione;
+import it.unicam.cs.ids.hackhub.entity.Sottomissione;
+import it.unicam.cs.ids.hackhub.repository.RepoSottomissione;
+
+@Service
+public class ServiceSottomissione {
+
+    private final RepoSottomissione repoSottomissione;
+
+    public ServiceSottomissione(RepoSottomissione repoSottomissione) {
+        this.repoSottomissione = repoSottomissione;
+    }
+
+    public boolean sottomissioneConsentita(Iscrizione iscrizione) {
+        if (iscrizione == null) {
+            return false;
+        }
+        return iscrizione.getHackathon().sottomissioneConsentita();
+    }
+
+    public boolean inviaSottomissione(Iscrizione iscrizione, String titolo, String contenuto) {
+        if (iscrizione == null) {
+            return false;
+        }
+        if (!iscrizione.getHackathon().sottomissioneConsentita()) {
+            return false;
+        }
+        if (titolo == null || titolo.isEmpty()) {
+            return false;
+        }
+        if (contenuto == null) {
+            return false;
+        }
+
+        Sottomissione sub = iscrizione.creaSottomissione(titolo, contenuto);
+        repoSottomissione.salva(sub);
+        return true;
+    }
+}
